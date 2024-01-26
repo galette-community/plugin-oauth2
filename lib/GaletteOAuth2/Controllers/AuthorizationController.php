a1c00f1b9dab96d36920a649f2eea7c6fd7f4546
Johan Cwiklinski <johan@x-tnd.be>
2024-01-26 07:05:13 +0100

    Add and check header

--- lib/GaletteOAuth2/Controllers/AuthorizationController.php
+++ lib/GaletteOAuth2/Controllers/AuthorizationController.php
@@ -1,34 +1,25 @@
 <?php
 
-declare(strict_types=1);
-
 /**
- * Plugin OAuth2 for Galette Project
+ * Copyright © 2021-2024 The Galette Team
  *
- *  PHP version 7
+ * This file is part of Galette OAuth2 plugin (https://galette-community.github.io/plugin-oauth2/).
  *
- *  This file is part of 'Plugin OAuth2 for Galette Project'.
+ * Galette is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  Plugin OAuth2 for Galette Project is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Plugin OAuth2 for Galette Project is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Galette is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with Plugin OAuth2 for Galette Project. If not, see <http://www.gnu.org/licenses/>.
- *
- *  @category Plugins
- *  @package  Plugin OAuth2 for Galette Project
- *
- *  @author    Manuel Hervouet <manuelh78dev@ik.me>
- *  @copyright Manuel Hervouet (c) 2021
- *  @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0
+ * You should have received a copy of the GNU General Public License
+ * along with Galette OAuth2 plugin. If not, see <http://www.gnu.org/licenses/>.
  */
+
+declare(strict_types=1);
 
 namespace GaletteOAuth2\Controllers;
 
@@ -45,6 +36,12 @@ use League\OAuth2\Server\Exception\OAuthServerException;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
+/**
+ * Controller for authorization
+ *
+ * @author Manuel Hervouet <manuelh78dev@ik.me>
+ * @author Johan Cwiklinski <johan@x-tnd.be>
+ */
 final class AuthorizationController extends AbstractPluginController
 {
     /**
